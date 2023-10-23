5e04f5d8088c374bc106911a8766558599738e2f
niabalaji <73157615+niabalaji12@users.noreply.github.com>
2023-10-23 13:38:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Nia Balaji" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          hi! contact me <code>pages/index.js</code>
         </p>
       </main>
 
